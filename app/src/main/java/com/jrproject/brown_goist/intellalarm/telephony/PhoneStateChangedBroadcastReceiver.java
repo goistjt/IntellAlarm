88a5dda81c45fdd11bc1102e5e8059113af9e0c7
goistjt <goistjt@rose-hulman.edu>
2015-11-25 21:22:41 +0100

    Finalizing Code cleanup

--- app/src/main/java/com/jrproject/brown_goist/intellalarm/telephony/PhoneStateChangedBroadcastReceiver.java
+++ app/src/main/java/com/jrproject/brown_goist/intellalarm/telephony/PhoneStateChangedBroadcastReceiver.java
@@ -16,12 +16,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class PhoneStateChangedBroadcastReciever extends BroadcastReceiver {
+public class PhoneStateChangedBroadcastReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.d(getClass().getSimpleName(), "onReceive()");
-
 	}
 
 }
