51be5a20c39599e80fb6bd87c433c51f606016e4
goistjt <goistjt@rose-hulman.edu>
2015-12-13 13:28:52 +0100

    Running analysis of incoming sensor events in SleepActivity Reduces data stored in DB Counts number of movements over calibrated threshold Store 1 minute at a time

--- app/src/main/java/com/jrproject/brown_goist/intellalarm/SensorData.java
+++ app/src/main/java/com/jrproject/brown_goist/intellalarm/SensorData.java
@@ -1,14 +1,9 @@
 package com.jrproject.brown_goist.intellalarm;
 
-/**
- * Created by goistjt on 11/29/2015.
- */
 public class SensorData implements Comparable<SensorData>{
 
     private int id;
-    private float xValue;
-    private float yValue;
-    private float zValue;
+    private int numEvents;
     private long timeStamp;
 
     public SensorData() {
@@ -23,28 +18,12 @@ public class SensorData implements Comparable<SensorData>{
         this.id = id;
     }
 
-    public float getxValue() {
-        return xValue;
+    public int getNumEvents() {
+        return numEvents;
     }
 
-    public void setxValue(float xValue) {
-        this.xValue = xValue;
-    }
-
-    public float getyValue() {
-        return yValue;
-    }
-
-    public void setyValue(float yValue) {
-        this.yValue = yValue;
-    }
-
-    public float getzValue() {
-        return zValue;
-    }
-
-    public void setzValue(float zValue) {
-        this.zValue = zValue;
+    public void setNumEvents(int numEvents) {
+        this.numEvents = numEvents;
     }
 
     public long getTimeStamp() {
